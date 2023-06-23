452d0700bcc488f2fba89c6b112aa22c3abd39fb
Matthieu Symoens <matthieu.symoens@gmail.com>
2023-06-23 07:02:28 -0400

    Go

--- bin.js
+++ bin.js
@@ -1,1 +1,35 @@
 #! /usr/bin/env node
+const fs = require('fs-extra');
+const parseArgs = require('yargs-parser');
+
+const exit = (code) => process.exit(code);
+const writeLn = (s) => process.stdout.write(`${s}\n`);
+const parseBoolean = (b) => b === true || b === 1;
+const parseString = (s) => typeof s === 'boolean' ? '' : (s === 0 ? '0' : String(s || ''));
+
+const help = `Usage:
+npx @ape-framework/starter <name> [--ts | --js] [-f | --force]
+
+Options:
+--ts         TypeScript template (default).
+--js         JavaScript template.
+-f, --force  Remove existing project.`;
+
+const options = parseArgs(process.argv.slice(2));
+const args = options._;
+delete options._;
+
+const name = parseString(args[0]);
+if (!name || name === 'help') { writeLn(help); exit(); }
+
+const typescript = parseBoolean(options.ts);
+const javascript = parseBoolean(options.js);
+
+const force = parseBoolean(options.f) || parseBoolean(options.force);
+
+// throw new Error(`CLI: invalid shit "${shit}"`);
+// const foo = fs.existsSync('package.json');
+
+console.log(options);
+console.log(args);
+console.log({ typescript, javascript, force });
